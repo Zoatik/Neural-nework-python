a64b388e2b5ca28703723b51a3daa4cc7c2ae461
Zoatik <axelhal@hotmail.com>
2024-03-21 22:22:29 +0100

    Modification de la structure (suppression des layers et création d'une liste 2d network)

--- neural_network.py
+++ neural_network.py
@@ -1,88 +1,117 @@
-from cmath import tanh
+from cmath import tanh, exp
 import random as rd
 
+def sigmoid(x, derivate = False):
+    if derivate:
+        return exp(x)/(1+exp(-x))**2
+    return 1/(1+exp(-x))
+def th(x, derivate = False):
+    if derivate:
+        return 1-tanh(x)**2
+    return tanh(x)
+def leakyReLu(x, eps = 0, derivate = False):
+    if derivate:
+        if x < 0:
+            return  eps
+        return 1
+    return max(eps,x)
+class actFunc:
+    def __init__(self, funcName):
+        if funcName == "tanh":
+            self.func = tanh
+        elif funcName == "sigmoid":
+            self.func = sigmoid
+        elif funcName == "leakyReLu":
+            self.func = leakyReLu
+    
+    
 class Neur:
     def __init__(self, val = 0):
-        self.biais = rd.uniform(0.0,1.0)
+        self.biais = 0.0
         self.val = val
         self.weightIndexes = []
-        self.back_links = []
+        self.backLinks = []
         
     def calcVal(self, givenVal, weight):
         return givenVal*weight + self.biais
+        
     def activate(self, fct, new_val):
         self.val = fct(new_val)
         return self.val
     
-class Layer:
-    def __init__(self, size, list = []):
-        self.neur_list = []
-        self.size = size
-        if(list == []):
-            for i in range(size):
-                self.neur_list.append(Neur())
-        else:
-            for i in range(size):
-                self.neur_list.append(Neur(list[i]))
-    def print(self):
-        for i in range(self.size):
-            print(self.neur_list[i].val)
                     
             
 class Network:
-    def __init__(self):
+    def __init__(self, fct = actFunc("tanh")):
         self.nbLayers = 0
+        self.network = [[]]
         self.layers = []
-        self.fct = tanh
+        self.fct = fct
+    
+    def createLayer(self, list):
+        layer = []
+        for val in list:
+            layer.append(Neur(val))
+        return layer
+    
+    def print(self):
+        for layer in self.network:
+            for neur in layer:
+                print(neur.backLinks)
         
-    def setLayers(self, nbHiddenLayers, in_layerSize, out_layerSize, hiddenLayerSize):
+    def setNetwork(self, nbHiddenLayers, out_layerSize, hiddenLayerSize):
         self.nbLayers = nbHiddenLayers + 2
-        self.layers.append(Layer(in_layerSize))
-        for i in range(nbHiddenLayers):
-            self.layers.append(Layer(hiddenLayerSize))
-        self.layers.append(Layer(out_layerSize))
+        self.network += [[Neur()]*hiddenLayerSize]*nbHiddenLayers
+        self.network += [[Neur()]*out_layerSize]
         
     def setActivFunc(self,fct):
         self.fct = fct
         
     def initWeights(self):
         for i in range(1, self.nbLayers):
-            for j in range(self.layers[i].size):
-                for k in range(self.layers[i-1].size):
-                    self.layers[i].neur_list[j].back_links.append(rd.uniform(0.0,1.0))
+            for j in range(len(self.network[i])):
+                for k in range(len(self.network[i-1])):
+                    self.network[i][j].backLinks.append(rd.uniform(0.0,1.0))
                     
-    def process(self, in_data):
-        fstLayer = Layer(len(in_data), in_data)
-        self.layers[0] = fstLayer
+    def forwProp(self, in_data):
+        in_dataLayer = self.createLayer(in_data)
+        self.network[0] = in_dataLayer + self.network
         for i in range(1, self.nbLayers):
-            for j in range(self.layers[i].size):
+            for j in range(len(self.network[i])):
                 tmp_val = 0
-                for k in range(len(self.layers[i].neur_list[j].back_links)):
-                    weight = self.layers[i].neur_list[j].back_links[k]
-                    tmp_val += self.layers[i].neur_list[j].calcVal(self.layers[i].neur_list[j].val, weight)
-                print("new val: ", self.layers[i].neur_list[j].activate(self.fct, tmp_val))
-        out_data = self.layers[-1]
-        return out_data
+                for k in range(len(self.network[i][j].backLinks)):
+                    weight = self.network[i][j].backLinks[k]
+                    tmp_val += self.network[i][j].calcVal(self.network[i-1][j].val, weight)
+                self.network[i][j].activate(self.fct, tmp_val)
+            
+        return self.network[-1]
     
     def showLinks(self):
-        for layer in self.layers:
-            for neur in layer.neur_list:
-                print(neur.back_links)
+        for layer in self.network:
+            for neur in layer:
+                print(neur.backLinks)
                 
     def showNeurInfos(self, layer, index):
-        print(self.layers[layer].neur_list[index].back_links)
-                    
+        print(self.network[layer][index].backLinks)
+
 network = Network()
-network.setLayers(3,4,1,3)
+network.setNetwork(50,2,3)
 network.setActivFunc(tanh)
 network.initWeights()
 
-network.showLinks()
+#network.showLinks()
 input_list  = [1,2,3,4]
-layer_out = network.process(input_list)
-layer_out.print()    
+nbIterations = int(input("nombre d'itérations : "))
+
+for i in range(nbIterations):
+    layer_out = network.forwProp(input_list)
+    print([neur.val for neur in layer_out])
                 
         
         
         
         
+
+
+
+    
