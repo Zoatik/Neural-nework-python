50bdef82a64693ba4b9a6479066b8cf07447b206
Zoatik <axelhal@hotmail.com>
2024-03-27 22:49:18 +0100

    essai de faire converger

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,4 @@
-from math import tanh, exp
+from math import tanh, exp, sqrt, log
 import random as rd
 
 def sigmoid(x, derivate = False):
@@ -9,16 +9,16 @@ def th(x, derivate = False):
     if derivate:
         return 1-tanh(x)**2
     return tanh(x)
-def leakyReLu(x, eps = 0, derivate = False):
+def leakyReLu(x, eps = 0.01, derivate = False):
     if derivate:
-        if x < 0:
+        if x <= 0:
             return  eps
-        return 1
-    return max(eps,x)
+        return 1/(x+1)
+    return log(x+1)+eps
 class ActFunc:
     def __init__(self, funcName):
         if funcName == "tanh":
-            self.func = tanh
+            self.func = th
         elif funcName == "sigmoid":
             self.func = sigmoid
         elif funcName == "leakyReLu":
@@ -35,8 +35,8 @@ class Neur:
     def calcVal(self, givenVal, weight):
         return givenVal*weight + self.biais
         
-    def activate(self, fct, new_val):
-        self.val = fct(new_val)
+    def activate(self, fct, new_val, derivate = False):
+        self.val = fct(new_val, derivate)
         return self.val
     
                     
@@ -93,10 +93,32 @@ class Network:
                 for k in range(len(self.network[i-1])):
                     weight = self.network[i][j].linksWeight[k]
                     tmp_val += self.network[i][j].calcVal(self.network[i-1][k].val, weight)
-                self.network[i][j].activate(self.fct, tmp_val)
+                    #if i == 1:
+                        #print("tmp val: ",tmp_val)
+                        #print(self.network[i][j].calcVal(self.network[i-1][k].val, weight))
+                        #print("val: ", self.network[i-1][k].val)
+                if i == self.nbLayers-1 or i == 1:
+                    self.network[i][j].val = tmp_val
+                else:
+                    self.network[i][j].activate(self.fct, tmp_val)
             
         return self.network[-1]
     
+    def backProp(self, delta_0, rate):
+        for i in range(self.nbLayers-2,-1,-1):
+            for j in range(len(self.network[i+1])):
+                for k in range(len(self.network[i])):
+                    W = self.network[i+1][j].linksWeight[k]
+                    #print(i)
+                    Z = self.network[i][k].val
+                    delta = W * delta_0 * self.fct(Z, True)
+                    #print("derivate fct", self.fct(Z, True))
+                    #print("delta: ",delta)
+                    #print("Weight: ", W)
+                    #print("val : ", Z)
+                    #print("rate : ", rate)
+                    self.network[i+1][j].linksWeight[k] = abs(W - rate * Z * delta)
+
     def showLinks(self):
         for layer in self.network:
             for neur in layer:
@@ -104,16 +126,29 @@ class Network:
                 
     def showNeurInfos(self, layer, index):
         print(self.network[layer][index].linksWeight)
+        
+    def showNeurVals(self):
+        for layer in self.network:
+            for neur in layer:
+                print(neur.val, end = ' ')
+            print()
+            
+    def showLayerVals(self, layer):
+        for neur in layer:
+            print("[",neur.val,"]")
 
 #####_Main_#####
 #setting up the network
 network = Network()
-network.setNetwork(3,2,10)
+network.setNetwork(4,1,50)
 network.setActivFunc("leakyReLu")
 
-input_list  = [1,2,3,4]
-network.setInput(input_list)
-network.print()
+#input_list  = [1,2,3,4]
+train_list = [[i]for i in range(1,5)]
+expect_out_list = [[i] for i in range(2,9,2)]
+rate = 0.00001
+network.setInput(train_list[0])
+#network.print()
 network.initWeights()
 print()
 network.print()
@@ -121,7 +156,26 @@ network.print()
 nbIterations = int(input("nombre d'itérations : "))
 
 for i in range(nbIterations):
-    layer_out = network.forwProp()
+    global_error = 0
+    for j in range(len(train_list)):
+        layer_out = network.forwProp()
+        network.network[0] = network.createLayer(train_list[j])
+        print(i+1, "/", nbIterations, ": DONE")
+        print(['%.2f' % neur.val for neur in layer_out])
+        error_list = []
+        for k in range(len(layer_out)):
+            error_list.append(sqrt((layer_out[k].val - expect_out_list[j][k])**2))
+        print("error list: ", error_list)
+        global_error += sum(error_list)/len(layer_out)
+    print("global error : ", global_error)
+        #network.print()
+        #print("\n\n")
+    network.backProp(global_error, rate)
+
+while(True):
+    new_input = float(input("nombre à évaluer: "))
+    network.network[0][0] = Neur(new_input)
+    print("in neur: ", network.network[0][0].val)
+    network.showLayerVals(network.forwProp())
+    #network.showNeurVals()
     
-    print(i+1, "/", nbIterations, ": DONE")
-    print(['%.2f' % neur.val for neur in layer_out])
